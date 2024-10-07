92423d666b6a14df0a6e91a46ea34cc7b6a8baa3
Luna-Notes <support@lunanotes.co.in>
2024-10-07 12:06:24 +0530

    last server change
    last server change

---
+++ picresizeserver/resizeserver/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+import os
+from django.conf import settings
+
+
+class ResizeserverConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'resizeserver'
