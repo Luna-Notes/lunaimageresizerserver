8ec65fade1b7d150295cf54f3a63e69ec9951261
Luna-Notes <support@lunanotes.co.in>
2024-10-14 18:29:17 +0530

    Delete picresizeserver directory

--- picresizeserver/resizeserver/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class ResizeImageSerializer(serializers.Serializer):
-    image = serializers.ImageField(required=True)
-    width = serializers.IntegerField(required=True, min_value=100)
-    height = serializers.IntegerField(required=True, min_value=100)
